04c0ab774136dc37817b8e57bf9ad10f5d84a2b4
Aidan Trimmer <trimmeag@dukes.jmu.edu>
2022-10-10 13:24:18 -0400

    adding js functions

--- js/functions.js
+++ js/functions.js
@@ -6,11 +6,30 @@ function calculateTotal(q, p) {
   return q * p;
 }
 
-function outputCartRow(file, title, quantity, price, total) {
-  return " " + item + " " + total + " ";
+function outputCartRow(item, total) {
+  console.log(item, total);
+  const {
+    product: { title, filename, price },
+    quantity,
+  } = item;
+  const row = `         <tr>
+    <td><img src="images/${filename}"></td>
+    <td>${title}</td>
+    <td>${quantity}</td>
+    <td>${price}</td>
+    <td>${total}</td>
+    </tr>`;
+  document.write(row);
 }
 
-for(let item of cart) {
+function outputTableBody() {
+  for (let item of cart) {
     let total = calculateTotal(item.quantity, item.product.price);
     outputCartRow(item, total);
+  }
 }
+
+function outputTotalRow(name, total) {
+
+}
+
