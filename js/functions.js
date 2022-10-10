6855d815ec31ad107a23fd78dd57243af6fb7326
Aidan Trimmer <trimmeag@dukes.jmu.edu>
2022-10-10 13:10:31 -0400

    first edits

--- js/functions.js
+++ js/functions.js
@@ -1,10 +1,16 @@
 /* define your functions here */
 
+console.log(cart);
 
+function calculateTotal(q, p) {
+  return q * p;
+}
 
+function outputCartRow(file, title, quantity, price, total) {
+  return " " + item + " " + total + " ";
+}
 
-
-
-
-
-        
+for(let item of cart) {
+    let total = calculateTotal(item.quantity, item.product.price);
+    outputCartRow(item, total);
+}
